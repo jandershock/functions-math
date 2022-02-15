4e96d744a7a266a96553db2efee8afb9b9721141
jandershock <jordan.andershock@gmail.com>
2022-02-15 15:33:08 -0600

    Adding and multiplication works.

---
+++ main.js
@@ -0,0 +1,14 @@
+//Add two numbers, return result
+const addStuff = (firstVal, secondVal) => {
+    return firstVal + secondVal;
+}
+
+//Multiply by 2, return result
+const multByTwo = (someVal) => {
+    return someVal * 2;
+}
+
+
+let x = addStuff(4,14);
+x = multByTwo(x);
+console.log(x);
